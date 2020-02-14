a1edbf16f5ef33a2d074caaa3f23d020cb9175ed
FizeChan <chenfengzhan@qq.com>
2020-02-14 16:11:21 +0800

    完成"一次性订阅消息"接口。

--- src/offiaccount/message/Template.php
+++ src/offiaccount/message/Template.php
@@ -3,8 +3,46 @@
 
 namespace fize\third\wechat\offiaccount\message;
 
+use fize\third\wechat\Offiaccount;
 
-class Template
+/**
+ * 订阅模板消息
+ */
+class Template extends Offiaccount
 {
 
+    /**
+     * 推送订阅模板消息
+     * @param string $touser 接收消息的用户openid
+     * @param int $scene 订阅场景值
+     * @param string $template_id 订阅消息模板ID
+     * @param string $title 消息标题
+     * @param array $data 消息正文
+     * @param string $url 点击消息跳转的链接
+     * @param string|array $miniprogram 小程序
+     * @return bool
+     */
+    public function subscribe($touser, $scene, $template_id, $title, array $data, $url = null, $miniprogram = null)
+    {
+        $params = [
+            'touser'      => $touser,
+            'scene'       => $scene,
+            'template_id' => $template_id,
+            'title'       => $title,
+            'data'        => $data
+        ];
+        if (!is_null($url)) {
+            $params['url'] = $url;
+        }
+        if (is_null($miniprogram)) {
+            if (is_string($miniprogram)) {
+                $miniprogram = [
+                    'appid' => $miniprogram
+                ];
+                $params['miniprogram'] = $miniprogram;
+            }
+        }
+        $result = $this->httpPost("/message/template/subscribe?access_token={$this->accessToken}", $params);
+        return $result ? true : false;
+    }
 }
---
+++ src/offiaccount/mp/Subscribemsg.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace fize\third\wechat\offiaccount\mp;
+
+use fize\third\wechat\Offiaccount;
+
+/**
+ * 一次性订阅信息
+ */
+class Subscribemsg extends Offiaccount
+{
+
+    /**
+     * 获取授权链接
+     * @param int $scene 订阅场景值
+     * @param string $template_id 模板ID
+     * @param string $redirect_url 跳转URI
+     * @param string $reserved 原样返回
+     * @return string
+     */
+    public function getConfirm($scene, $template_id, $redirect_url, $reserved = null)
+    {
+        $redirect_url = urlencode($redirect_url);
+        $uri = "https://mp.weixin.qq.com/mp/subscribemsg?action=get_confirm&appid={$this->appid}&scene={$scene}&template_id={$template_id}&redirect_url={$redirect_url}";
+        if (!is_null($reserved)) {
+            $uri .= "&reserved={$reserved}";
+        }
+        $uri .= "#wechat_redirect";
+        return $uri;
+    }
+}
