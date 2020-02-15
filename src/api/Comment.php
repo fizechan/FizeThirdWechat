274975c3d61a6d58efd55607da9f8b6699a950ba
FizeChan <chenfengzhan@qq.com>
2020-02-15 22:53:12 +0800

    修正“图文消息留言管理”功能。

--- src/api/Comment.php
+++ src/api/Comment.php
@@ -1,21 +1,21 @@
 <?php
 
 
-namespace fize\third\wechat\offiaccount;
+namespace fize\third\wechat\api;
 
-use fize\third\wechat\Offiaccount;
+use fize\third\wechat\Api;
 
 /**
  * 图文消息留言管理
  */
-class Comment extends Offiaccount
+class Comment extends Api
 {
 
     /**
      * 打开已群发文章评论
      * @param int $msg_data_id 群发返回的msg_data_id
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return array|false
+     * @return array
      */
     public function open($msg_data_id, $index = null)
     {
@@ -32,7 +32,7 @@ class Comment extends Offiaccount
      * 关闭已群发文章评论
      * @param int $msg_data_id 群发返回的msg_data_id
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return array|false
+     * @return array
      */
     public function close($msg_data_id, $index = null)
     {
@@ -52,7 +52,7 @@ class Comment extends Offiaccount
      * @param int $count 获取数目（>=50会被拒绝）
      * @param int $type type=0 普通评论&精选评论 type=1 普通评论 type=2 精选评论
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return array|false
+     * @return array
      */
     public function list($msg_data_id, $begin, $count, $type, $index = null)
     {
@@ -73,7 +73,6 @@ class Comment extends Offiaccount
      * @param int $msg_data_id 群发返回的msg_data_id
      * @param int $user_comment_id 用户评论id
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return bool
      */
     public function markelect($msg_data_id, $user_comment_id, $index = null)
     {
@@ -84,8 +83,7 @@ class Comment extends Offiaccount
         if (!is_null($index)) {
             $params['index'] = $index;
         }
-        $result = $this->httpPost("/comment/markelect?access_token={$this->accessToken}", $params);
-        return $result ? true : false;
+        $this->httpPost("/comment/markelect?access_token={$this->accessToken}", $params);
     }
 
     /**
@@ -93,7 +91,6 @@ class Comment extends Offiaccount
      * @param int $msg_data_id 群发返回的msg_data_id
      * @param int $user_comment_id 用户评论id
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return bool
      */
     public function unmarkelect($msg_data_id, $user_comment_id, $index = null)
     {
@@ -104,8 +101,7 @@ class Comment extends Offiaccount
         if (!is_null($index)) {
             $params['index'] = $index;
         }
-        $result = $this->httpPost("/comment/unmarkelect?access_token={$this->accessToken}", $params);
-        return $result ? true : false;
+        $this->httpPost("/comment/unmarkelect?access_token={$this->accessToken}", $params);
     }
 
     /**
@@ -113,7 +109,6 @@ class Comment extends Offiaccount
      * @param int $msg_data_id 群发返回的msg_data_id
      * @param int $user_comment_id 用户评论id
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return bool
      */
     public function delete($msg_data_id, $user_comment_id, $index = null)
     {
@@ -124,7 +119,6 @@ class Comment extends Offiaccount
         if (!is_null($index)) {
             $params['index'] = $index;
         }
-        $result = $this->httpPost("/comment/delete?access_token={$this->accessToken}", $params);
-        return $result ? true : false;
+        $this->httpPost("/comment/delete?access_token={$this->accessToken}", $params);
     }
 }
--- src/api/comment/Reply.php
+++ src/api/comment/Reply.php
@@ -1,14 +1,14 @@
 <?php
 
 
-namespace fize\third\wechat\offiaccount\comment;
+namespace fize\third\wechat\api\comment;
 
-use fize\third\wechat\Offiaccount;
+use fize\third\wechat\Api;
 
 /**
  * 评论回复
  */
-class Reply extends Offiaccount
+class Reply extends Api
 {
     /**
      * 回复评论
@@ -16,7 +16,6 @@ class Reply extends Offiaccount
      * @param int $user_comment_id 用户评论id
      * @param string $content 回复内容
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return bool
      */
     public function add($msg_data_id, $user_comment_id, $content, $index = null)
     {
@@ -28,8 +27,7 @@ class Reply extends Offiaccount
         if (!is_null($index)) {
             $params['index'] = $index;
         }
-        $result = $this->httpPost("/comment/reply/add?access_token={$this->accessToken}", $params);
-        return $result ? true : false;
+        $this->httpPost("/comment/reply/add?access_token={$this->accessToken}", $params);
     }
 
     /**
@@ -37,7 +35,6 @@ class Reply extends Offiaccount
      * @param int $msg_data_id 群发返回的msg_data_id
      * @param int $user_comment_id 用户评论id
      * @param int $index 用来指定第几篇图文，从0开始
-     * @return bool
      */
     public function delete($msg_data_id, $user_comment_id, $index = null)
     {
@@ -48,7 +45,6 @@ class Reply extends Offiaccount
         if (!is_null($index)) {
             $params['index'] = $index;
         }
-        $result = $this->httpPost("/comment/reply/delete?access_token={$this->accessToken}", $params);
-        return $result ? true : false;
+        $this->httpPost("/comment/reply/delete?access_token={$this->accessToken}", $params);
     }
 }
