2dc2f35dedbd3d04319acbc3d66ea87bfdc548f3
FizeChan <chenfengzhan@qq.com>
2020-02-22 23:30:34 +0800

    完成接口“智能接口”。

--- src/api/media/Voice.php
+++ src/api/media/Voice.php
@@ -1,12 +1,58 @@
 <?php
 
 
-namespace fize\third\wechat\offiaccount\media;
+namespace fize\third\wechat\api\media;
+
+use CURLFile;
+use fize\third\wechat\Api;
 
 /**
  * 语音
  */
-class Voice
+class Voice extends Api
 {
 
+    /**
+     * 提交语音
+     * @param string $file 语音MP3文件
+     * @param string $voice_id 唯一标识
+     * @param string $lang 语言
+     */
+    public function addvoicetorecofortext($file, $voice_id, $lang = 'zh_CN')
+    {
+        $params = [
+            'media' => new CURLFile(realpath($file))
+        ];
+        $this->httpPost("/media/voice/addvoicetorecofortext?access_token={$this->accessToken}&format=&voice_id={$voice_id}&lang={$lang}", $params, false);
+    }
+
+    /**
+     * 获取语音识别结果
+     * @param string $voice_id 唯一标识
+     * @param string $lang 语言
+     * @return string
+     */
+    public function queryrecoresultfortext($voice_id, $lang = 'zh_CN')
+    {
+        $params = [
+            'voice_id' => $voice_id,
+            'lang' => $lang
+        ];
+        $result = $this->httpPost("/media/voice/queryrecoresultfortext?access_token={$this->accessToken}", $params, false);
+        return $result['result'];
+    }
+
+    /**
+     * 微信翻译
+     * @param string $file 语音MP3文件
+     * @param string $lfrom 源语言
+     * @param string $lto 目标语言
+     */
+    public function translatecontent($file, $lfrom, $lto)
+    {
+        $params = [
+            'media' => new CURLFile(realpath($file))
+        ];
+        $this->httpPost("/media/voice/translatecontent?access_token={$this->accessToken}&lfrom={$lfrom}&lto={$lto}", $params, false);
+    }
 }
--- src/api/semantic/Semproxy.php
+++ src/api/semantic/Semproxy.php
@@ -1,12 +1,31 @@
 <?php
 
 
-namespace fize\third\wechat\offiaccount\semantic;
+namespace fize\third\wechat\api\semantic;
+
+use fize\third\wechat\Api;
 
 /**
  * 语义理解
  */
-class Semproxy
+class Semproxy extends Api
 {
 
+    /**
+     * 发送语义理解请求
+     * @param string $query 输入字符串
+     * @param string $category 需要使用的服务类型
+     * @param array $extend 其他字段
+     * @return array
+     */
+    public function search($query, $category, array $extend)
+    {
+        $params = [
+            'query' => $query,
+            'category' => $category,
+            'appid' => $this->appid
+        ];
+        $params = array_merge($params, $extend);
+        return $this->httpPost("/semantic/semproxy/search?access_token={$this->accessToken}", $params);
+    }
 }
