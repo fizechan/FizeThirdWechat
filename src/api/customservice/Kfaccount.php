a8d07c50f4a5db728683382a86ea4e57f942347a
FizeChan <chenfengzhan@qq.com>
2020-02-22 23:55:29 +0800

    完成接口“新版客服功能”。

--- src/api/customservice/Kfaccount.php
+++ src/api/customservice/Kfaccount.php
@@ -14,14 +14,14 @@ class Kfaccount extends Api
 
     /**
      * 添加客服账号
-     * @param string $account 完整客服账号，格式为：账号前缀@公众号微信号，账号前缀最多10个字符，必须是英文或者数字字符
+     * @param string $kf_account 完整客服账号，格式为：账号前缀@公众号微信号，账号前缀最多10个字符，必须是英文或者数字字符
      * @param string $nickname 客服昵称，最长6个汉字或12个英文字符
      * @param string $password 客服账号明文登录密码，会自动加密
      */
-    public function add($account, $nickname, $password)
+    public function add($kf_account, $nickname, $password)
     {
         $params = [
-            "kf_account" => $account,
+            "kf_account" => $kf_account,
             "nickname"   => $nickname,
             "password"   => md5($password)
         ];
@@ -29,39 +29,35 @@ class Kfaccount extends Api
     }
 
     /**
-     * 修改客服账号信息
+     * 邀请绑定客服帐号
+     * @param string $kf_account 完整客服帐号
+     * @param string $invite_wx 接收绑定邀请的客服微信号
+     */
+    public function inviteworker($kf_account, $invite_wx)
+    {
+        $params = [
+            "kf_account" => $kf_account,
+            "invite_wx"   => $invite_wx
+        ];
+        $this->httpPost("/customservice/kfaccount/inviteworker?access_token={$this->accessToken}", $params);
+    }
+
+    /**
+     * 设置客服信息
      * @param string $account 完整客服账号，格式为：账号前缀@公众号微信号，账号前缀最多10个字符，必须是英文或者数字字符
      * @param string $nickname 客服昵称，最长6个汉字或12个英文字符
-     * @param string $password 客服账号明文登录密码，会自动加密
      */
-    public function update($account, $nickname, $password)
+    public function update($account, $nickname)
     {
         $params = [
             "kf_account" => $account,
-            "nickname"   => $nickname,
-            "password"   => md5($password)
+            "nickname"   => $nickname
         ];
         $this->httpPost("/customservice/kfaccount/update?access_token={$this->accessToken}", $params);
     }
 
     /**
-     * 删除客服帐号
-     * @param string $account 完整客服账号，格式为：账号前缀@公众号微信号，账号前缀最多10个字符，必须是英文或者数字字符
-     * @param string $nickname 客服昵称，最长6个汉字或12个英文字符
-     * @param string $password 客服账号明文登录密码，会自动加密
-     */
-    public function del($account, $nickname, $password)
-    {
-        $params = [
-            "kf_account" => $account,
-            "nickname"   => $nickname,
-            "password"   => md5($password)
-        ];
-        $this->httpPost("/customservice/kfaccount/del?access_token={$this->accessToken}", $params);
-    }
-
-    /**
-     * 设置客服帐号的头像
+     * 上传客服头像
      * @param string $kf_account 完整客服账号，格式为：账号前缀@公众号微信号，账号前缀最多10个字符，必须是英文或者数字字符
      * @param string $file 头像文件完整路径,如：'D:\user.jpg'。头像文件必须JPG格式，像素建议640*640
      */
@@ -71,5 +67,17 @@ class Kfaccount extends Api
             'media' => new CURLFile(realpath($file))
         ];
         $this->httpPost("/customservice/kfaccount/uploadheadimg?access_token={$this->accessToken}&kf_account={$kf_account}", $params, false);
+    }
+
+    /**
+     * 删除客服帐号
+     * @param string $kf_account 完整客服账号
+     */
+    public function del($kf_account)
+    {
+        $params = [
+            "kf_account" => $kf_account
+        ];
+        $this->httpPost("/customservice/kfaccount/del?access_token={$this->accessToken}", $params);
     }
 }
--- src/api/customservice/Msgrecord.php
+++ src/api/customservice/Msgrecord.php
@@ -1,12 +1,32 @@
 <?php
 
 
-namespace fize\third\wechat\offiaccount\customservice;
+namespace fize\third\wechat\api\customservice;
+
+use fize\third\wechat\Api;
 
 /**
  * 聊天记录
  */
-class Msgrecord
+class Msgrecord extends Api
 {
 
+    /**
+     * 获取聊天记录
+     * @param int $starttime 起始时间
+     * @param int $endtime 结束时间
+     * @param int $msgid 消息id顺序从小到大，从1开始
+     * @param int $number 每次获取条数
+     * @return array
+     */
+    public function getmsglist($starttime, $endtime, $msgid, $number)
+    {
+        $params = [
+            'starttime' => $starttime,
+            'endtime'   => $endtime,
+            'msgid'     => $msgid,
+            'number'    => $number
+        ];
+        return $this->httpPost("/customservice/msgrecord/getmsglist?access_token={$this->accessToken}", $params);
+    }
 }
